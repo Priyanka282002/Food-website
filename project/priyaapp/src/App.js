949fe8d33388dd871ec2d4cf124754021e301877
Priyanka <gpriyankagupta54@gmail.com>
2023-02-13 11:02:01 +0530

    Food Menu Card

---
+++ project/priyaapp/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+// import Resturant from "./component/Basics/Resturant";cd 
+// import UseState from "./component/Hooks/useState";
+// import UseEffect from "./component/Hooks/useEffect";
+import Restaurant from "Restaurant.js";
+
+const App = () => {
+  return <UseReducer />;
+};
+
+export default App;
+
+
